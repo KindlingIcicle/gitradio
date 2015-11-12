6d414f16b5cf997682fd9b0a743c598e4c9afbae
way0750 <bruce0750@gmail.com>
2015-11-11 16:05:47 -0800

    initial set up for server and client server set up: 	dependencies: expressjs, body-parser client set up: 	dependencies: backbonejs, underscorejs 	assets: main.css 	index.html

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(express.static('../client'));
+
+var testHandler = function (req, res, next) {
+  console.log(req.method);
+  res.status(200).send('works?');
+};
+
+app.get(testHandler);
+
+
+var port = 8888;
+
+app.listen(port);
+
+console.log('server running at:', port);
