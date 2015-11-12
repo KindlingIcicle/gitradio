67557cec8bf8156237d909e9acca38b428f093fa
Charles Harrington <charles.harrington@gmail.com>
2015-11-12 11:56:44 -0800

    (feature) Set up route for webhook on server

--- server/server.js
+++ server/server.js
@@ -4,18 +4,32 @@ var path = require('path');
 var app = express();
 
 //serve up static directory
-app.use(express.static('./client'));
-
+// Note: adjusted the directory location to work locally
+//       May not work on heroku unless revert to './client'
+app.use(express.static('../client'));
 
 var testHandler = function (req, res, next) {
   console.log(req.method);
   res.status(200).send('works?');
 };
 
-app.get(testHandler);
+// set up parser for webhook
+var jsonParser = bodyParser.json();
 
-var port = process.env.PORT || 8888;
+// handles the github callback POST
+// This can be tested with running server with 'ngrok'
+var githubHandler = function (req, res, next) {
+  console.log(req.body);
+  res.status(200).send("Thank you!");
+};
 
+//-----------POST---------------------//
+app.post('/githubCallbackURL', jsonParser, githubHandler);
+
+//-----------GET----------------------//
+app.get('/', testHandler);
+
+//-----------SETUP---------------------//
+var port = process.env.PORT || 5123;
 app.listen(port);
-
 console.log('server running at:', port);
