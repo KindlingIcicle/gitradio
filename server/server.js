9261d621026fb895d5962a826c32b39c2587fe24
Charles Harrington <charles.harrington@gmail.com>
2015-11-12 12:17:29 -0800

    (feature) create event object for each event coming through the webhook

--- server/server.js
+++ server/server.js
@@ -19,7 +19,24 @@ var jsonParser = bodyParser.json();
 // handles the github callback POST
 // This can be tested with running server with 'ngrok'
 var githubHandler = function (req, res, next) {
-  console.log(req.body);
+  var head = req.headers;
+  var body = req.body;
+  var time = new Date();
+
+  // create "event" object that can be saved in database, or
+  // streamed to client via sockets
+  var event = {
+    type: head['x-github-event'],
+    time: time,
+    user: body.sender.login,
+    user_url: body.sender.url,
+    user_avatar_url: body.sender.avatar_url,
+    repo: body.repository.name
+  };
+
+  console.log(event);
+  
+  // tell Github thank you
   res.status(200).send("Thank you!");
 };
 
