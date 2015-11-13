b83dc8677fe706c9caef8c8e0bdfa50dd8ac8f77
Banun Atina Idris <banunatina@gmail.com>
2015-11-12 20:27:07 -0800

    (feat) Integrate sockets and webhooks.

---
+++ server/io.js
@@ -0,0 +1,10 @@
+var io = require('../app');
+
+io.on('connection', function(socket){
+  console.log('connected user!');
+  socket.on('looping', function() {
+    console.log('Server heard it!');
+    io.emit('looping');
+  });
+
+});
