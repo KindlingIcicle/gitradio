aeac3a0b2b22f6aa5dff074360b60f9c54c256d7
way0750 <bruce0750@gmail.com>
2015-11-16 00:39:11 -0800

    edited the style: 	added a baner 	removed the text shadow of the quote

--- public/client/views/eventEntryView.js
+++ public/client/views/eventEntryView.js
@@ -2,7 +2,7 @@ var eventEntryView  = Backbone.View.extend({
   tagName : 'li',
   template : _.template('<div class = "eventEntry">\
     <img class = "img-circle" src = <%=  data.user_avatar_url %> >\
-    <p><a href="<%= data.user_url %>"><b><%= data.user %></b>:</a></p>\
+    <p class = "userName"><a href="<%= data.user_url %>"><b><%= data.user %></b>:</a></p>\
     <p><a class = "eventType" href="<%= data.repo_url %>"><%=data.type%></p></a>\
     <p class ="eventTime" ><%=data.time%></p>\
     </div>'),
@@ -15,7 +15,8 @@ var eventEntryView  = Backbone.View.extend({
     //should return the $el since it is so versatile
     var dataToDisplay = this.template(this.model.attributes); //{data : event}
     this.$el.append(dataToDisplay);
+    // this.$el.slideUp(0);
+    // this.$el.slideDown(600);
     return this.$el;
   }
-
 });
