07a82ec3766516cbeb41c3039500915e5ec711ae
Banun Atina Idris <banunatina@gmail.com>
2015-11-16 16:41:47 -0800

    (feat) Refactor sound.js to backbone model.

--- public/client/models/app.js
+++ public/client/models/app.js
@@ -1,13 +1,8 @@
 var app = Backbone.Model.extend({
-  //might be useful later on.
   //for now the app model is only a container for the eventCollection
-  
-  audioLibChange : function () {
-    console.log("i heard this in the app model but what do I do now?");
-  },
 
 //to receive data and pushing data to the eventCollection
-  initialize : function(){
+  initialize : function(params){
 
 //connect sockets on initialize
     var socket = io();
@@ -16,18 +11,24 @@ var app = Backbone.Model.extend({
     var newEvent = this.addNewEvent.bind(this);
 
     socket.on('event', function(event) {
-      console.log('heard the event');
-      //if the appropriate sound for the event is available, play it
-      if (loaded[event.type]) {
-        playSound(loaded[event.type]);
+
+//if the appropriate sound for the event is available, play it
+    var lib = params.library.get('loaded');
+      if (lib[event.type]) {
+        params.library.playSound(lib[event.type]);
       } else {
-        //play default (note: to be refactored to default)
-        playSound(loaded['pull_denied']);
+        //play default
+        params.library.playSound(lib['default']);
       }
       
 //update the feed
       newEvent({data:event});
     });
+   
+//listening for change to soundLibrary
+   params.library.on('change:soundLibrary', function(data) {
+     params.library.loadLibrary(data.get('soundLibrary'));
+   }, this);
 
   },
 
