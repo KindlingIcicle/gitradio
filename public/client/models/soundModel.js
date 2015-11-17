b8e0f3b749970a27a14042a082b6000428e7bcd7
Banun Atina Idris <banunatina@gmail.com>
2015-11-16 16:50:38 -0800

    (fix) Clean up code and add in-line comments.

--- public/client/models/soundModel.js
+++ public/client/models/soundModel.js
@@ -1,47 +1,66 @@
 var soundModel = Backbone.Model.extend({
-  initialize: function(params) {
-    this.set('soundLibrary', 'default');
 
+  initialize: function(params) {
+    //sets soundLibrary (directory name in assets) to default
+    this.set('soundLibrary', 'default');
+ 
+    //initializes new AudioContext for Web Audio API
     this.set('context', function() {
       return new AudioContext();
     }());
 
+   //initializes object to be accessed by playSound
     this.set('loaded', {});
+
+   //sets data to data passed into soundModel
     this.set('data', params.data);
+
+    //loads current library
     this.loadLibrary(this.get('soundLibrary'));
   },
 
-  changeLib : function () {
-    this.set('soundLibrary', 'allen');
-  },
-
+//initializes bufferLoader to be used by loadSounds
   bufferLoader: null,
 
- //LOAD THE CURRENT LIBRARY
+//Function to toggle between sound libraries - TODO: ability to switch between more than two
+  changeLib : function () {
+    if (this.get('soundLibrary') === 'default') {
+      this.set('soundLibrary', 'allen');
+    } else {
+      this.set('soundLibrary', 'default');
+    }
+  },
+
+
+ //Load the current library - calls loadSounds
   loadLibrary : function (lib) {
     var context = this.get('context');
     var data = this.get('data');
     this.loadSounds(context, data, lib);
   },
 
-//Function to load sounds - takes audio context, a soundMap
+//Loads sounds - takes audio context, a soundMap and a library (points to dirname in assets)
   loadSounds: function (context, soundMap, lib) {
-  
+  //initializes names and paths - TODO: use names to assign in storeSounds
     var names = [];
     var paths = [];
 
+  //pushes all paths from soundMap to paths
     for (var name in soundMap) {
       var path = soundMap[name].path;
       var event = soundMap[name].event;
       names.push(name);
+      //puts together path for the proper asset directory and asset file
       paths.push(path + lib + event);
     }
 
-
+  //store AudioContext to be used inside Buffer Loader
    var context = this.get('context');
 
+  //bind storeSounds function to this instance of songModel
    var storeSounds = this.storeSounds.bind(this);
 
+  //sets bufferLoader to a newly instantiated Buffer Loader
     this.set('bufferLoader', function(context, paths, callback) {
       return new BufferLoader(
       context,
@@ -50,11 +69,12 @@ var soundModel = Backbone.Model.extend({
       );
     }(context, paths, storeSounds));
 
+  //load
     this.get('bufferLoader').load();
   },
 
+  //stores sound in model's loaded object - TODO: refactor to use names in loadSounds somehow
   storeSounds : function (bufferList) {
-    console.log(this);
     this.set('loaded', {
       'pull_request' : bufferList[0],
       'fork' : bufferList[1],
@@ -63,6 +83,7 @@ var soundModel = Backbone.Model.extend({
     });
   },
 
+  //plays the sound requested - TODO: disconnect all nodes upon library reinstantiation
   playSound : function (buffer) {
     var source = this.get('context').createBufferSource();
     source.buffer = buffer;
